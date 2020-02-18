bfb7e8b4b9693b74086e74928e2714d8d2995cc3
Ricky Hsu <rickyhsu@qnap.com>
2020-02-18 17:59:16 +0800

    Finish linecount && checksum

---
+++ go.mod
@@ -0,0 +1,9 @@
+module CLI-fops
+
+go 1.13
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v0.0.5
+	golang.org/x/tools v0.0.0-20200216192241-b320d3a0f5a2
+)
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"CLI-fops/cli"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("$ You could use [help] to get some instruction or [exit] to leave the terminal.")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("$ ")
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		cmdString = strings.TrimSuffix(cmdString, "\n")
+		arrCommandStr := strings.Fields(cmdString)
+
+		newOsArgs := []string{os.Args[0]}
+		newOsArgs = append(newOsArgs, arrCommandStr...)
+		os.Args = newOsArgs
+
+		if len(os.Args) == 1{
+			continue
+		}
+
+		switch os.Args[1] {
+		case "help":
+			os.Args = []string{os.Args[0], "-h"}
+			cli.Execute()
+		case "exit":
+			cli.Exit()
+		default:
+			cli.Execute()
+		}
+	}
+}
